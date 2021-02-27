97cb09f4cb54face42cc29f651d4993ff8ace46d
Ebers-Hyde <tharuthlessman@gmail.com>
2021-02-27 13:26:27 +0300

    v 1.1 добавил более явную блокировку выбора недоступного времени в полях отправки, произвёл немного рефакторинга

--- js/main.js
+++ js/main.js
@@ -44,23 +44,27 @@ window.addEventListener("load", function() {
 
     //обработчик смены значения в поле маршрута
     routePoint.addEventListener('change', function() {
+        //Каждый раз при смене направления запускается функция, возвращающая блоки с выбором времени в изначальное состояние
+        //Обнуляются значения в полях количества билетов и убирается информация о прошлом заказе. 
+        resetRestrictions(routeTime1.options);
+        resetRestrictions(routeTime2.options);
+        regularTicketsInput.value = '';
+        childrenTicketsInput.value = '';
+        infoBlock.style.display = "none";
+        //если осталось предупреждение из-за выбранного раньше маршрута в два конца, скрывается.
+        if(validator.textContent) {
+            validator.textContent = "";
+        }
         //значение записывается в переменную
         routePointVal = this.value;
         //в зависимости от выбранного маршрута скрывается или показывается нужный список для выбора времени
         if (routePointVal == "из A в B") {
-            //если осталось предупреждение из-за выбранного раньше маршрута в два конца, скрывается.
-            if(validator.textContent) {
-                validator.textContent = "";
-            }
             //через свойство стиля display список с временем отбытия показывается или скрывается
             routeTime1.style.display = "block";
             routeTime1Label.style.display = "block";
             routeTime2.style.display = "none";
             routeTime2Label.style.display = "none";
         } else if (routePointVal == "из B в A") {
-            if(validator.textContent) {
-                validator.textContent = "";
-            }
             routeTime2.style.display = "block";
             routeTime2Label.style.display = "block";
             routeTime1.style.display = "none";
@@ -76,23 +80,31 @@ window.addEventListener("load", function() {
     //обработчик клика по кнопке расчёта
     calcButton.addEventListener("click", function(evt) {
         evt.preventDefault();
+
+        //Каждый раз при нажатии кнопки "Рассчитать" блок с информацией о предыдущем заказе убирается,
+        //чтобы он не оставался при недотсупности нового маршрута. 
+        infoBlock.style.display = "none";
+
+        //Значения полей времени
+        let routeTime1Val = routeTime1.value;
+        let routeTime2Val = routeTime2.value;
         //вот тут срабатывает проверка на допустимость выбранного времени в обратную сторону.
         if(routePointVal == "из A в B и обратно") {
-            let routeTime1Val = routeTime1.value;
-            let routeTime2Val = routeTime2.value;
-           
             //вызывается функция проверки временных меток
             if(!compareTime(routeTime2Val, routeTime1Val, 50)) {
-                //если выбранная временная метка отправления  в обратную сторону отличается от отправки из изначального места меньше, чем на 50 минут, показывается валидатор.
+                //если выбранная временная метка отправления  в обратную сторону отличается от отправки из изначального места меньше, чем на 50 минут, показывается валидатор,
+                //блокируется текущая временная метка, а пльзователь видит "Время недоступно"
                 validator.textContent = "Пожалуйста, выберите другое время обратного рейса!";
+                routeTime2.options[routeTime2.selectedIndex].textContent = "Время недоступно";
+                routeTime2.options[routeTime2.selectedIndex].disabled = true;
             } else {
                 validator.textContent = "";
             }
         } else if (routePointVal == "из B в A и обратно") {
-            let routeTime1Val = routeTime1.value;
-            let routeTime2Val = routeTime2.value;
             if(!compareTime(routeTime1Val, routeTime2Val, 50)) {
                 validator.textContent = "Пожалуйста, выберите другое время обратного рейса!";
+                routeTime1.options[routeTime1.selectedIndex].textContent = "Время недоступно";
+                routeTime1.options[routeTime1.selectedIndex].disabled = true;
             } else {
                 validator.textContent = "";
             };
@@ -117,52 +129,40 @@ window.addEventListener("load", function() {
                 " по маршруту " + routePointVal + " стоимостью " + ticketsSum + " рублей." + "\n";
                 string += "Это путешествие займёт у вас";
 
+                //Время отправки и прибытия из А в B
+                let startTimeAB = moment(routeTime1.value);
+                let finishTimeAB = moment(routeTime1.value).add(50, "minutes");
+                startTimeAB = startTimeAB.format("HH:mm");
+                finishTimeAB = finishTimeAB.format("HH:mm");
+
+                //Время отправки и прибытия из B в A
+                let startTimeBA = moment(routeTime2.value);
+                let finishTimeBA = moment(routeTime2.value).add(50, "minutes");
+                startTimeBA = startTimeBA.format("HH:mm");
+                finishTimeBA = finishTimeBA.format("HH:mm");
+
+                //Дальнейшее формирование строки со временем туда (и обратно, если выбрано и установлено)
                 if(routePointVal == "из A в B и обратно") {
-                    let startTimeToDisplay1 = moment(routeTime1.value);
-                    let startTimeToDisplay2 = moment(routeTime1.value).add(50, "minutes");
-                    startTimeToDisplay1 = startTimeToDisplay1.format("HH:mm");
-                    startTimeToDisplay2 = startTimeToDisplay2.format("HH:mm");
-                    let backTimeToDisplay1 = moment(routeTime2.value);
-                    let backTimeToDisplay2 = moment(routeTime2.value).add(50, "minutes");
-                    backTimeToDisplay1 = backTimeToDisplay1.format("HH:mm");
-                    backTimeToDisplay2 = backTimeToDisplay2.format("HH:mm");
                     string += " по 50 минут в одну и другую сторону.\n";
-                    string += "Теплоход " + "из A в B" + " отправляется в " + startTimeToDisplay1 + " , а прибудет в " +
-                    startTimeToDisplay2 + ".";
-                    string += " Из B в A теплоход отправится в " + backTimeToDisplay1 + ", а прибудет в " + backTimeToDisplay2 + ".";
+                    string += "Теплоход " + "из A в B" + " отправляется в " + startTimeAB + " , а прибудет в " +
+                    finishTimeAB + ".";
+                    string += " Из B в A теплоход отправится в " + startTimeBA + ", а прибудет в " + finishTimeBA + ".";
 
                 } else if (routePointVal == "из B в A и обратно") {
-                    let startTimeToDisplay1 = moment(routeTime2.value);
-                    let startTimeToDisplay2 = moment(routeTime2.value).add(50, "minutes");
-                    startTimeToDisplay1 = startTimeToDisplay1.format("HH:mm");
-                    startTimeToDisplay2 = startTimeToDisplay2.format("HH:mm");
-                    let backTimeToDisplay1 = moment(routeTime1.value);
-                    let backTimeToDisplay2 = moment(routeTime1.value).add(50, "minutes");
-                    backTimeToDisplay1 = backTimeToDisplay1.format("HH:mm");
-                    backTimeToDisplay2 = backTimeToDisplay2.format("HH:mm");
                     string += " по 50 минут в одну и другую сторону.\n";
-                    string += "Теплоход " + "из B в A" + " отправляется в " + startTimeToDisplay1 + " , а прибудет в " +
-                    startTimeToDisplay2 + ".";
-                    string += " Из A в B теплоход отправится в " + backTimeToDisplay1 + ", а прибудет в " + backTimeToDisplay2 + ".";
+                    string += "Теплоход " + "из B в A" + " отправляется в " + startTimeBA + " , а прибудет в " +
+                    finishTimeBA + ".";
+                    string += " Из A в B теплоход отправится в " + startTimeAB + ", а прибудет в " + finishTimeAB + ".";
                 } else if (routePointVal == "из A в B") {
-                    let startTimeToDisplay1 = moment(routeTime1.value);
-                    let startTimeToDisplay2 = moment(routeTime1.value).add(50, "minutes");
-                    startTimeToDisplay1 = startTimeToDisplay1.format("HH:mm");
-                    startTimeToDisplay2 = startTimeToDisplay2.format("HH:mm");
-                    string += " 50 минут. Теплоход отправляется в " + startTimeToDisplay1 + ", а прибудет в " + startTimeToDisplay2 + ".";
+                    string += " 50 минут. Теплоход отправляется в " + startTimeAB + ", а прибудет в " + finishTimeAB + ".";
                 } else if (routePointVal == "из B в A") {
-                    let startTimeToDisplay1 = moment(routeTime2.value);
-                    let startTimeToDisplay2 = moment(routeTime2.value).add(50, "minutes");
-                    startTimeToDisplay1 = startTimeToDisplay1.format("HH:mm");
-                    startTimeToDisplay2 = startTimeToDisplay2.format("HH:mm");
-                    string += " 50 минут. Теплоход отправляется в " + startTimeToDisplay1 + ", а прибудет в " + startTimeToDisplay2 + ".";
+                    string += " 50 минут. Теплоход отправляется в " + startTimeBA + ", а прибудет в " + finishTimeBA + ".";
                 }
                 
                 tripInfo.textContent = string;
         }
     });
 });
-
 
 //Функция перевода отметки времени в выбранный локальный часовой пояс, принимает массив с метками и количество часов разницы с Москвой
 
@@ -194,3 +194,12 @@ let compareTime = (firstTimeStamp, secondTimeStamp, difference) => {
     }
 }
 
+//Функция возврата полей выбора времени к обычному состоянию (все доступны, у всех значения со временем)
+
+let resetRestrictions = arr => {
+    for(let i=0; i <= arr.length - 1; i++) {
+        arr[i].disabled = false;
+        let option = moment(arr[i].value);
+        arr[i].textContent = option.format("HH:mm");
+    }
+}
